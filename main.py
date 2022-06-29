d99ddf9247bb442db5d6810ba9860ed73f54ccd7
David Aydin <david.aydin6@gmail.com>
2022-06-28 23:06:55 -0400

    Exercise 1, prompt user and return vowel or consonant based off input

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Write the code that:
 # 1. Prompts the user to enter a letter in the alphabet:
-#      Please enter a letter from the alphabet (a-z or A-Z): 
+#      Please enter a letter from the alphabet (a-z or A-Z):
 # 2. Write the code that determines whether the letter entered is a vowel
 # 3. Print one of following messages (substituting the letter for x):
 #      - The letter x is a vowel
@@ -10,26 +10,28 @@
 
 # Hints:  Use the in operator to check if a character is in another string
 #         For example, if some_char in 'abc':
-
-
+letter = input('Please enter a letter from the alphabet (a-z or A-Z).\n')
+if letter in 'a e i o u':
+    print(f'The letter {letter} is a vowel')
+else:
+    print(f'The letter {letter} is a consonant')
 
 
 # exercise-02 Length of Phrase
 
 # Write the code that:
 # 1. Prompts the user to enter a phrase:
-#      Please enter a word or phrase: 
+#      Please enter a word or phrase:
 # 2. Print the following message:
 #      - What you entered is xx characters long
 # 3. Return to step 1, unless the word 'quit' was entered.
-
 
 
 # exercise-03 Calculate Dog Years
 
 # Write the code that:
 # 1. Prompts the user to enter a dog's age in human years like this:
-#      Input a dog's age in human years: 
+#      Input a dog's age in human years:
 # 2. Calculates the equivalent dog years, where:
 #      - The first two years count as 10 years each
 #      - Any remaining years count as 7 years each
@@ -44,7 +46,7 @@
 # Write the code that:
 # 1. Prompts the user to enter the three lengths of a triangle (one at a time):
 #      Enter the lengths of three side of a triangle:
-#      a: 
+#      a:
 #      b:
 #      c:
 # 2. Write the code that determines if the triangle is:
@@ -53,8 +55,6 @@
 #      isosceles - two sides are the same length
 # 3. Print a message such as:
 #      - A triangle with sides of <a>, <b> & <c> is a <type of triangle> triangle
-
-
 
 
 # exercise-05 Fibonacci sequence for first 50 terms
@@ -73,15 +73,12 @@
 # Hint: The next number is found by adding the two numbers before it
 
 
-
-
-
 # exercise-06 What's the  Season?
 
 # Write the code that:
 # 1. Prompts the user to enter the month (as three characters):
 #      Enter the month of the season (Jan - Dec):
-# 2. Then propts the user to enter the day of the month: 
+# 2. Then propts the user to enter the day of the month:
 #      Enter the day of the month:
 # 3. Calculate what season it is based upon this chart:
 #      Dec 21 - Mar 19: Winter
@@ -89,7 +86,7 @@
 #      Jun 21 - Sep 21: Summer
 #      Sep 22 - Dec 20: Fall
 # 4. Print the result as follows:
-#      <Mmm> <dd> is in <season> 
+#      <Mmm> <dd> is in <season>
 
 # Hints:
 # Consider using the in operator to check if a string is in a particular list/tuple like this:
